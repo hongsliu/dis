308ce85381ce79f69000ce01e749f3978df85bc6
Hongsheng Liu <hongs.liu@outlook.com>
2021-08-05 15:52:19 +0800

    regression

--- regression.R
+++ regression.R
@@ -122,10 +122,89 @@ glance(model4)
 
 model5 <- Regressiondata %>%
   lm(pub_per ~
-       one_car_or_more_per_2011+
-       level_4_per_2011,
+       pop_den_2011+
+       log_median_house_price_2012+
+       level_4_per_2011+
+       log_pub_tra_stops_den,
      data=.)
 summary(model5)
 tidy(model5)
 glance(model5)
+
+library(tidypredict)
+Regressiondata %>%
+  tidypredict_to_column(model2)
+
+model_data <- model2 %>%
+  augment
+
+model_data%>%
+  dplyr::select(.resid)%>%
+  pull()%>%
+  qplot()+ 
+  geom_histogram() 
+
+#and for future use, write the residuals out
+model_data2 <- model2 %>%
+  augment(., Regressiondata)
+
+# also add them to the shapelayer
+LonmsoaProfiles <- LonmsoaProfiles %>%
+  mutate(model2resids = residuals(model2))
+
+vif(model2)
+
+#print some model diagnositcs. 
+par(mfrow=c(2,2))    #plot to 2 by 2 array
+plot(model2)
+
+par(mfrow=c(1,1)) 
+
+DW <- durbinWatsonTest(model2)
+tidy(DW)
+
+#now plot the residuals
+tmap_mode("view")
+#qtm(LonmsoaProfiles, fill = "model2_resids")
+
+tm_shape(LonmsoaProfiles) +
+  tm_polygons("model2resids",
+              palette = "RdYlBu")
+
+#calculate the centroids of all Wards in London
+coordsM <- LonmsoaProfiles%>%
+  st_centroid()%>%
+  st_geometry()
+
+plot(coordsM)
+
+#Now we need to generate a spatial weights matrix (remember from the lecture a couple of weeks ago). We'll start with a simple binary matrix of queen's case neighbours
+
+Lmsoa_nb <- LonmsoaProfiles %>%
+  poly2nb(., queen=T)
+
+#or nearest neighbours
+knn_msoa <-coordsM %>%
+  knearneigh(., k=4)
+
+Lmsoa_knn <- knn_msoa %>%
+  knn2nb()
+
+#plot them
+plot(Lmsoa_nb, st_geometry(coordsM), col="red")
+plot(Lmsoa_knn, st_geometry(coordsM), col="blue")
+
+#create a spatial weights matrix object from these weights
+
+Lmsoa.knn_4_weight <- Lmsoa_knn %>%
+  nb2listw(., style="C")
+
+Nearest_neighbour <- LonmsoaProfiles %>%
+  st_drop_geometry()%>%
+  dplyr::select(model2resids)%>%
+  pull()%>%
+  moran.test(., Lmsoa.knn_4_weight)%>%
+  tidy()
+
+Nearest_neighbour
 
